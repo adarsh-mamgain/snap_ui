f98fc2a2439bbbea90b4a9fdf80cd630c0d3d8ed
Adarsh Mamgain <52105807+adarsh-mamgain@users.noreply.github.com>
2025-03-14 17:09:33 +0530

    refactor: Remove SnapButton and SnapCard components from the library to streamline the UI component offerings

--- lib/snap_ui.dart
+++ lib/snap_ui.dart
@@ -2,9 +2,5 @@
 export 'src/config/snap_config.dart';
 export 'src/config/snap_constants.dart';
 
-// Export components
-export 'src/components/button.dart';
-export 'src/components/card.dart';
-
 // Export examples
 export 'src/examples/config_example.dart';
